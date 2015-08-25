c4d124f0e33e6f5e72fef0cacae1781214a5b7cb
Patriot <patriot@izhevsk.ru>
2015-08-25 11:59:55 +0400

    nested list

--- nestedlist1.py
+++ nestedlist1.py
@@ -2,8 +2,11 @@ __author__ = 'patriot'
 
 """
 Problem Statement
-Now we will see how to implement a nested list. There is a classroom of 'n' students and you are given their names and marks in physics. Store them in a nested list and print the name of each student who got the second lowest marks in physics.
-NOTE: If there are more than one student getting same marks, make sure you print the names of all students in alphabetical order, in different lines.
+Now we will see how to implement a nested list. There is a classroom of 'n' students
+and you are given their names and marks in physics. Store them in a nested list and
+print the name of each student who got the second lowest marks in physics.
+NOTE: If there are more than one student getting same marks, make sure you print
+the names of all students in alphabetical order, in different lines.
 
 Input Format
 Integer $N$ followed by alternating sequence of N strings and N numbers.
@@ -32,13 +35,33 @@ Sample Output
 Berry
 Harry
 """
+"""
+marksheet = []
+for _ in range(0, int(input())):
+    marksheet.append([input(), float(input())])
 
-N = int(raw_input())
+second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
+print('\n'.join([a for a, b in sorted(marksheet) if b == second_highest]))
+"""
+
 l = []
-for i in range(N):
-    p = [raw_input()]
-    p.append(float(raw_input()))
-    l.append(p)
-    p = []
+for i in range(int(raw_input())):
+    l.append([raw_input(), float(raw_input())])
 l.sort()
-print(l)
+l2 = sorted(l, key=lambda st: st[1])
+mi = l2[0][1]
+l2.pop(0)
+while len(l2) > 0:
+    if l2[0][1] != mi:
+        print l2[0][0]
+        mi2 = l2[0][1]
+        l2.pop(0)
+        while len(l2) > 0:
+            if l2[0][1] == mi2:
+                print l2[0][0]
+                l2.pop(0)
+            else:
+                break
+        break
+    else:
+        l2.pop(0)

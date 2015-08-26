24804f3db4c5373a9bcb3be773da7553c11a813a
Patriot <patriot@izhevsk.ru>
2015-08-26 17:29:30 +0400

    string split join

---
+++ string-split-join1.py
@@ -0,0 +1,25 @@
+__author__ = 'patriot'
+
+"""
+Task
+You are given a string. Split the string on " " (space) delimiter and join using a - hiphen.
+
+Input Format
+The first line contains a string consisting of words separated by space.
+
+Output Format
+Print the formatted string as explained above.
+
+Sample Input
+
+this is a string
+
+Sample Output
+
+this-is-a-string
+"""
+
+S = raw_input()
+l = S.split(" ")
+St = '-'.join(l)
+print St
